d570b613a5266196b01cd6b16d1ca53069e31d26
Ian Jeremy Guani <ianguani@yahoo.ca>
2022-09-06 08:56:51 -0700

    downloaded plugins and fixed checkstyle.

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,13 @@
+package org.comp2522;
+
+/**
+ * class main.
+ */
+public class Main {
+  /**
+   * prints the line "corgi".
+   */
+  public static void main(String[] args) {
+    System.out.println("corgi");
+  }
+}
